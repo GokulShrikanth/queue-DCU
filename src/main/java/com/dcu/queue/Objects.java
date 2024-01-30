7ee05e32a909b2b7fcf97d63f457501db595ba0f
Gokul Shrikanth <gokulshrikanth99@gmail.com>
2024-01-30 11:22:00 +0000

    added to string

--- src/main/java/com/dcu/queue/Objects.java
+++ src/main/java/com/dcu/queue/Objects.java
@@ -6,7 +6,11 @@ public class Objects {
     Objects(int i) {
         this.id = i;
     }
-
+    @Override
+    public String toString() {
+        return Integer.toString(this.id);
+    }
+    static Object obj = new Objects(1);
     public static void main(String[] args) {
         Queue<Objects> queue = new Queue<>();
         queue.enqueue(new Objects(1));
@@ -17,6 +21,7 @@ public class Objects {
 
         while (!queue.isEmpty()) {
             System.out.println(queue.dequeue().id);
+            System.out.println(obj.toString());
         }
     }
 }
